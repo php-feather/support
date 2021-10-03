38da77b4758ae07ef88c959447a6278c379dfbac
Francis Carbah <fcarbah@gmail.com>
2021-10-03 10:38:34 -0400

    added request parameters bag contract

---
+++ src/Support/Contracts/IRequestParamBag.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Feather\Support\Contracts;
+
+use Feather\Support\Util\Bag;
+
+/**
+ *
+ * @author fcarbah
+ */
+interface IRequestParamBag
+{
+
+    /**
+     *
+     * @param array $items
+     * @param bool $update
+     */
+    public function addItems(array $items, bool $update);
+
+    /**
+     *
+     * @param string $key
+     * @param mixed $default
+     * @return \Feather\Support\Util\Bag|mixed
+     */
+    public function all($key, $default);
+
+    /**
+     *
+     * @param string $key
+     * @param mixed $default
+     * @return \Feather\Support\Util\Bag|mixed
+     */
+    public function cookie($key, $default);
+
+    /**
+     *
+     * @param string $key
+     * @param mixed $default
+     * @return \Feather\Support\Util\Bag|mixed
+     */
+    public function get($key, $default);
+
+    /**
+     *
+     * @param string $key
+     * @param mixed $default
+     * @return Feather\Support\Util\Bag|null
+     */
+    public function post($key, $default);
+}
